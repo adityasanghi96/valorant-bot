f4a1aee8da74d85ff515ec289052f79ac5ec531c
adityasanghi96 <adityasanghi96@gmail.com>
2024-05-18 04:52:11 +0530

    Changes in console

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const HenrikDevValorantAPI = require('unofficial-valorant-api');
 const vapi = new HenrikDevValorantAPI(process.env.API_KEY);
 
 const fs = require('fs');
-const myConsole = new console.Console(fs.createWriteStream('./output.txt'));
 
 const rateLimit = require('express-rate-limit');
 
@@ -52,15 +51,15 @@ app.get('/valorant/:name/:tag', async (req, res, next) => {
         });
         if (mmr_data.error) {
             res.send(`Error ${mmr_data.status}`);
-            return myConsole.log(`Username: ${name}#${tag} Error: ${mmr_data.status}`);
+            return console.log(`Username: ${name}#${tag} Error: ${mmr_data.status}`);
         }
         res.send(`Current Rank: ${mmr_data.data.current_data.currenttierpatched} - ${mmr_data.data.current_data.ranking_in_tier} RR, Peak Rank: ${mmr_data.data.highest_rank.patched_tier}`);
-        myConsole.log(`${name}#${tag} [${mmr_data.data.currenttierpatched}] - ${mmr_data.data.ranking_in_tier} RR`);
+        console.log(`${name}#${tag} [${mmr_data.data.currenttierpatched}] - ${mmr_data.data.ranking_in_tier} RR`);
     } catch (error) {
         next(error);
     }
 });
 
 app.listen(port, () => {
-    myConsole.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
